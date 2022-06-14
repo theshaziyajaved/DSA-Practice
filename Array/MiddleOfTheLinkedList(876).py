39eab7f21b0a17ee1d0d0f79daeaeafd79b806e4
interns@panhealth.com <shaziyajaveddhn1999@gmail.com>
2022-06-14 18:49:46 +0530

    middleofthelinkedlist

---
+++ Array/MiddleOfTheLinkedList(876).py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+
+from typing import Optional
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        slow = head
+        fast = head
+        
+        while fast.next != None:
+            slow = slow.next
+            fast= fast.next
+            
+            if (fast.next != None):
+                fast = fast.next
+        return slow
+    
+    
+        
+        
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        count=0
+        temp = head
+        
+        while temp is not None:
+            count += 1
+            temp = temp.next
+        # print(count)
+        
+        temp = head
+        for i in range (0,count//2+1):
+            if (i== count//2):
+                return temp
+            temp = temp.next
+        return null
+            
+    
+    
