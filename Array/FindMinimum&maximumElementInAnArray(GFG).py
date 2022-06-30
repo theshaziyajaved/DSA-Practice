6d2bb056ecc7ee109804b8401b140e7aa88f0e53
interns@panhealth.com <shaziyajaveddhn1999@gmail.com>
2022-06-30 12:19:20 +0530

    findmin&max

---
+++ Array/FindMinimum&maximumElementInAnArray(GFG).py
@@ -0,0 +1,25 @@
+
+def getMinMax( a, n):
+    #O(nlogn) solution
+    # a = sorted(a)
+    # return a[0],a[len(a) - 1]
+    
+
+    min_ = a[1]
+    max_ = a[1]
+    
+    for i in range (len(a)):
+        if (a[i] >= max_):
+            max_ = a[i]
+        if (a[i] <= min_):
+            min_ = a[i]
+    return min_ , max_  
+        
+
+   
+    #     min_ = min(a)
+    #     max_ = max(a)
+    # return min_ , max_    
+        
+    
+    
