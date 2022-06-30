8126329f96cc9c3b3485008e8f47a27ef7362029
interns@panhealth.com <shaziyajaveddhn1999@gmail.com>
2022-06-30 21:57:43 +0530

    ReverseLinkedList

---
+++ LinkedList/ReverseLinkedList(Leetcode_ques206).py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from typing import Optional
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        temp = head
+        curr = head
+        prev = None
+        
+        while (temp != None):
+            temp = temp.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
+        
