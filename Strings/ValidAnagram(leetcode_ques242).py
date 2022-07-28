2d877527f0375ef71ba58562c9fdc5ce15ed3c0d
interns@panhealth.com <shaziyajaveddhn1999@gmail.com>
2022-07-28 12:12:30 +0530

    valid anagram

---
+++ Strings/ValidAnagram(leetcode_ques242).py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+         if len(s) != len(t):
+                return False
+            
+         if len(s) == len(t):
+            s = sorted(s)     
+            t = sorted(t)
+        
+            for i in range (len(s)):
+                if s[i] == t[i]:
+                    pass
+                elif s[i] != t[i]:
+                    return False
+            return True
+           
